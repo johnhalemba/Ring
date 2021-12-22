bebee990a489d057753790600ace89f9b0c1bec5
Jan Jakub Halemba <jhalemba@stud.elka.pw.edu.pl>
2021-12-22 12:17:40 +0100

    changes

--- Ring.h
+++ Ring.h
@@ -4,34 +4,197 @@
 
 #ifndef RING_RING_H
 #define RING_RING_H
-
-template <typename Key, typename Info>
-struct val {
-    Key key;
-    Info info;
-};
+#include <iostream>
+#include <exception>
+#include <random>
 
 template <typename Key, typename Info>
 class Ring {
-public:
-    friend class iterator;
-    class iterator {
-
-    };
-    Ring() = default;
-    ~Ring();
 private:
     struct Element {
-        val<Key, Info> values;
+    public:
+        Key key;
+        Info info;
         Element* left = nullptr;
         Element* right = nullptr;
+        Element(const Key& k, const Info& i) : key(k), info(i), left(nullptr), right(nullptr) {}
     };
-    Element* head = nullptr;
+    Element* root = nullptr;
+    int numberOfElements = 0;
+public:
+    Ring() = default;
+
+    ~Ring();
+    friend class iterator;
+    class iterator {
+    public:
+        friend class Ring<Key, Info>;
+        iterator() = default;
+        iterator(Element* pointer) { ptr = pointer; };
+        ~iterator() = default;
+        bool operator==(const iterator& rhs) { return ptr == rhs.ptr; };
+        bool operator!=(const iterator& rhs) { return ptr != rhs.ptr; };
+        iterator& operator++() { ptr = ptr->right; return (*this); };
+        iterator& operator++(int) { iterator temp(ptr); ptr = ptr->right; return temp; };
+        iterator& operator--() { ptr = ptr->left; return (*this); };
+        iterator& operator--(int) { iterator temp(ptr); ptr = ptr->left; return temp; };
+        iterator& operator=(const iterator& rhs) { if (this != &rhs) { ptr = rhs.ptr; } return (*this); };
+        Element& operator*() { return *ptr; };
+        Element* operator->() { return ptr; };
+
+    private:
+        Element* ptr = nullptr;
+    };
+    bool isLastElement();
+    iterator begin() { if (root == nullptr) return iterator(root); return iterator(root->right); };
+    iterator end() { return iterator(root); };
+    void push_front(const Key &k, const Info &i);
+    void deleteElement(iterator& itr);
+    void push_back(const Key &k, const Info &i);
+    Ring::iterator find(const Key& key, int occurance, bool& success, bool forwardDirection);
+    void pop_back();
+    void pop_front();
+    void insert_before(iterator &itr, const Key& k, const Info& i);
+    void insert_after(iterator &itr, const Key& k, const Info& i);
 };
+
 
 template<typename Key, typename Info>
 Ring<Key, Info>::~Ring() {
+    if (root == nullptr)
+        return;
+    while (!isLastElement())
+    {
+        pop_back();
+    }
+    delete root;
+}
 
+template<typename Key, typename Info>
+bool Ring<Key, Info>::isLastElement() {
+    return root == root->right;
+}
+
+
+template<typename Key, typename Info>
+typename Ring<Key, Info>::iterator Ring<Key, Info>::find(const Key &key, int occurance, bool &success, bool forwardDirection) {
+    if (occurance <= 0)
+        throw std::invalid_argument("Occurance is lower or equal to zero!");
+    int tempOcc = 0;
+    if (forwardDirection)
+    {
+        for (auto a = begin(); a != end(); a++)
+        {
+            if (a->key == key)
+            {
+                tempOcc++;
+                if (tempOcc == occurance)
+                {
+                    success = true;
+                    return a;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (auto a = end(); a != begin(); a--)
+        {
+            if (a->key == key)
+            {
+                tempOcc++;
+                if (tempOcc == occurance)
+                {
+                    success = true;
+                    return a;
+                }
+            }
+        }
+    }
+    success = false;
+    return iterator(nullptr);
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::push_front(const Key &k, const Info &i) {
+    if (root == nullptr)
+    {
+        auto temp = new Element(k, i);
+        root = temp;
+        temp -> left = root;
+        temp -> right = root;
+        numberOfElements++;
+        return;
+    }
+    auto itr = begin();
+    insert_before(itr, k, i);
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::insert_before(iterator &itr, const Key &k, const Info &i) {
+    auto temp = new Element(k, i);
+    temp->right = itr.ptr;
+    temp->left = itr.ptr->left;
+    //checking whether there is only one element in the list
+    if (itr.ptr == root->left) {
+        itr.ptr->left = temp;
+    }
+    itr.ptr->left = temp;
+    temp->left->right = temp;
+    numberOfElements++;
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::push_back(const Key &k, const Info &i) {
+    if (root == nullptr)
+    {
+        auto temp = new Element(k, i);
+        root = temp;
+        temp -> left = root;
+        temp -> right = root;
+        numberOfElements++;
+        return;
+    }
+    auto itr = end();
+    insert_before(itr, k, i);
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::deleteElement(Ring::iterator &itr) {
+    if (itr.ptr == root)
+        return;
+    auto temp = itr.ptr;
+    itr.ptr = temp->left;
+    temp->left->right = temp->right;
+    temp->right->left = temp->left;
+    delete temp;
+    numberOfElements--;
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::pop_back() {
+    iterator i(--end());
+    deleteElement(i);
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::pop_front() {
+    iterator i(begin());
+    deleteElement(i);
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::insert_after(Ring::iterator &itr, const Key &k, const Info &i) {
+    auto temp = new Element(k, i);
+    temp->right = itr.ptr->right;
+    temp->left = itr.ptr;
+    //checking whether there is only one element in the list
+    if (itr.ptr == root->left) {
+        itr.ptr->right = temp;
+    }
+    itr.ptr->right = temp;
+    temp->right->left = temp;
+    numberOfElements++;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@
 
 int main()
 {
+    Ring<int, int> r1;
+    r1.push_back(2, 3);
+    r1.push_front(5, 5);
+    r1.push_front(5, 6);
+    r1.push_back(9, 9);
+    auto it1 = r1.begin();
+    bool found;
+    it1++;
+    it1++;
+    r1.insert_after(it1, 10, 10);
+    r1.find(5, 2, found, true);
+    removeAllElements(r1, 5);
     return 0;
 }
 
